3bec1fd3a10cc75bba66b2f44943ac7ffb68629c
Ciro Santilli <ciro.santilli@gmail.com>
2017-05-15 08:28:17 +0100

    ident

--- hello/package/hello/hello.mk
+++ hello/package/hello/hello.mk
@@ -9,11 +9,11 @@ HELLO_SITE = $(BR2_EXTERNAL_HELLO_PATH)/package/hello/src
 HELLO_SITE_METHOD = local
 
 define HELLO_BUILD_CMDS
-    $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)
+	$(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)
 endef
 
 define HELLO_INSTALL_TARGET_CMDS
- $(INSTALL) -D -m 0755 $(@D)/hello $(TARGET_DIR)/usr/bin
+	$(INSTALL) -D -m 0755 $(@D)/hello $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
