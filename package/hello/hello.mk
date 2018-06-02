c8ba3a1ef35595201b8cc450570851254cc1d6c5
Ciro Santilli <ciro.santilli@gmail.com>
2018-06-03 00:44:04 +0100

    eigen

--- package/hello/hello.mk
+++ package/hello/hello.mk
@@ -7,7 +7,7 @@
 HELLO_VERSION = 1.0
 HELLO_SITE = ./package/hello/src
 HELLO_SITE_METHOD = local
-# HELLO_DEPENDENCIES += libdrm
+HELLO_DEPENDENCIES += eigen
 
 define HELLO_BUILD_CMDS
     $(MAKE) -C $(@D) $(TARGET_CONFIGURE_OPTS)
